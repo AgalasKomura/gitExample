4b1df6f5d68082ae872d02ff597d6bec9917ec0d
Den <Den@gmail.com>
2023-03-11 17:57:21 +0200

    Добавил int

---
+++ src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    static int a = 45;
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+        System.out.println("йцукен");
+        System.out.println(a);
+
+
+    }
+}
